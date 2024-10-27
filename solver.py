b2f0d5f6a010407d4e2d86916d323d69df91d5be
Enormax <petrpelikan0@gmail.com>
2024-10-27 13:28:36 +0100

    finished solver

--- solver.py
+++ solver.py
@@ -10,9 +10,44 @@
 # main.mainloop()
 
 import sys
+import os
+import Visualizer
 from Graph import Graph
 
-g = Graph.fromFile(sys.argv[1])
-print(g.vertices)
-print(g.edges)
-print(g.createCNFFormula())
+
+def printUsage():
+    print("Usage: solver.py <input file>")
+
+def createGraph():
+    return Graph.fromFile(sys.argv[1])
+
+def main():
+    if len(sys.argv) != 2:
+        print("Error: Incorrect number of arguments.")
+        printUsage()
+        return
+    
+    if not os.path.exists(sys.argv[1]):
+        print(f"Error: File {sys.argv[1]} doesn't exist.")
+        printUsage()
+        return
+    
+    g = Graph.fromFile(sys.argv[1])
+    cnf = g.createCNFFormula()
+    # print(g.vertices)
+    # print(g.edges)
+    # print(cnf)
+    
+    if os.path.exists("./tmp.in"):
+        os.remove("./tmp.in") # remove cnf file if it exists somehow
+    
+    with open("./tmp.in", "x") as f:
+        f.write(cnf) # write cnf formula to input file
+        
+    os.system("./glucose_static ./tmp.in ./tmp.out >/dev/null") # run glucose solver
+    os.remove("./tmp.in") # remove input file
+    
+    Visualizer.visualize(g, "./tmp.out")
+
+if __name__ == "__main__":
+    main()
